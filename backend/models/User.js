4cf2a05f2b9073c9db67e07b83fced7854645c94
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-04-16 09:08:47 +0530

    fix: collection name

--- backend/models/User.js
+++ backend/models/User.js
@@ -62,6 +62,6 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema, "eventix");
+const User = mongoose.model("User", userSchema);
 
 export default User;
